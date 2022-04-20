bb8962ac06262c07b3df29df6ba8e6ae9020c498
kevin <dkevinroth@gmail.com>
2022-04-20 12:30:29 -0300

    S31

--- Programacion con Java/Sobreescritura y Polimorfismo/src/test/TestSobreescritura.java
+++ Programacion con Java/Sobreescritura y Polimorfismo/src/test/TestSobreescritura.java
@@ -2,7 +2,7 @@ package test;
 
 import domain.*;
 
-public class TestSobrescritura {
+public class TestSobreescritura {
     public static void main(String[] args) {
         /*Gerente gerente1 = new Gerente("Juan",5000, "Sistemas");
         System.out.println("Gerente 1: " + gerente1.obtenerDetalles());*/
