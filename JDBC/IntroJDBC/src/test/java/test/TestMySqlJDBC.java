b3a2db7c77a966148a21f79a371d9609ec33566e
kevdroth <91353251+kevdroth@users.noreply.github.com>
2022-04-27 16:36:48 -0300

    S43

---
+++ JDBC/IntroJDBC/src/test/java/test/TestMySqlJDBC.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestMySqlJDBC {
+    public static void main(String[] args) {
+        var url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    }
+}
