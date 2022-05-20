20d04555625df30603707d8d9286227611deec83
kevin <dkevinroth@gmail.com>
2022-05-20 15:24:10 -0300

    S121

---
+++ Spring/ControlClientesInternacionalizacionSegJPA/src/main/java/ar/com/onwave/domain/Rol.java
@@ -0,0 +1,21 @@
+package ar.com.onwave.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Data //lombok - metodos y constructores
+@Entity
+@Table(name = "rol") //tabla a utilizar
+public class Rol implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //ai
+    private Long idRol;
+
+    @NotEmpty // validacion no vacio
+    private String nombre;
+}
