3039fd6f6c49b94281045b9edf1b639f3dc01cbe
kevin <dkevinroth@gmail.com>
2022-05-20 12:11:52 -0300

    S120

---
+++ Proyectos/ControlNumeracion/src/main/java/ar/com/onwave/controlnumeracion/ControlNumeracionApplication.java
@@ -0,0 +1,13 @@
+package ar.com.onwave.controlnumeracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlNumeracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlNumeracionApplication.class, args);
+	}
+
+}
---
+++ Proyectos/ControlNumeracion/src/test/java/ar/com/onwave/controlnumeracion/ControlNumeracionApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.onwave.controlnumeracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlNumeracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
