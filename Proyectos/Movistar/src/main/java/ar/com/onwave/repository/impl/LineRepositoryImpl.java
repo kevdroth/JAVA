a0b2d52551aefc48a8b8e3552fa8c558186276b1
kevin <dkevinroth@gmail.com>
2022-06-30 14:54:02 -0300

    Movistar

--- Proyectos/Movistar/src/main/java/ar/com/onwave/repository/impl/LineRepositoryImpl.java
+++ Proyectos/Movistar/src/main/java/ar/com/onwave/repository/impl/LineRepositoryImpl.java
@@ -3,6 +3,7 @@ package ar.com.onwave.repository.impl;
 import ar.com.onwave.repository.LineRepository;
 import ar.com.onwave.repository.mapper.Line.LineRowMapper;
 import ar.com.onwave.repository.model.LineModel;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,8 @@ import java.util.List;
 @Component
 public class LineRepositoryImpl implements LineRepository {
     private JdbcTemplate jdbcTemplate;
-    private static final String SQL_SELECT = "SELECT id_linea, id_plan, numero FROM linea";
+    private static final String SQL_SELECT = "SELECT linea.id_linea, linea.numero, plan.nombre" +
+            "FROM linea INNER JOIN plan ON linea.id_plan=plan.id_plan;";
     private static final String SQL_SELECT_BY_ID = "SELECT numero FROM linea WHERE id_linea = ?";
     private static final String SQL_INSERT = "INSERT INTO linea (id_plan, numero) VALUES (?,?)";
     private static final String SQL_DELETE = "DELETE FROM linea WHERE id_linea = ?";
